adadaa4f2233378af50f75d3e9dd67bc7abc534d
AliHassanL12 <liaqqahl@gmail.com>
2025-02-03 08:37:43 +0000

    Create gameboard, game and player object factories
    Set up the gameboard IIFE, to create the players and hold the
    gameboard. Then the game IIFE to play the game, and the createPlayer
    factory function to create instances of a player (we will only need 2).
    It is also being considered to shift the creation of players from the
    gameboard object to the game object.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,29 @@
-console.log('works');
+const gameboard = (function() {
+    const gameboardArray = []
+
+    const playerOne = createPlayer('X');
+    const playerTwo = createPlayer('Y');
+
+    const getPlayerOneMarker = () => playerOne.getMarker();
+    const getPlayerTwoMarker = () => playerTwo.getMarker();
+
+    return {
+        getPlayerOneMarker,
+        getPlayerTwoMarker
+    }
+})();
+
+const game = (function() {
+    
+})();
+
+function createPlayer(marker) {
+    this.marker = marker;
+
+    const getMarker = () => marker; 
+    return {
+        getMarker
+    }
+}
+
+
