ac5199f3a82c4f9adbabdc8222d0c2d6924ee05c
AliHassanL12 <liaqqahl@gmail.com>
2025-02-05 22:45:52 +0000

    Implement logic to check for row win
    We are checking if the current play that the user has made is a w-
    in. We don't bother to check the whole board, just based on what
    move the user has currently made. In the case that it is true we
    return the player that has won. Although, no implementations yet
    on resetting the board - so that will have to be made later, after
    vertical and diagonal checks are made.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,25 +1,29 @@
 const gameboard = (function() {
     const gameboardArray = [
-        '-','-','-',
-        '-','-','-',
-        '-','-','-',
+        ['-','-','-'],
+        ['-','-','-'],
+        ['-','-','-'],
     ];
 
     const resetBoard = () => gameboardArray = [];
 
     function display() {
-        console.log(gameboardArray)
+        console.log(gameboardArray);
     }
 
-    function placeMark(player, index) {
-        if ((index >= 1 && index <= 9) && (gameboardArray[index - 1] === '-')) {
-            gameboardArray[index - 1] = player; 
+    function placeMark(playerMarker, a, b) {
+        if ((a >= 0 && b >=0) && (a <= 3 && b <= 3) && (gameboardArray[a][b] === '-')) {
+            gameboardArray[a][b] = playerMarker;
             display();
         }
     }
 
-    function canPlace(index) {
-        if (gameboardArray[index - 1] === '-') return true;
+    function canPlace(a, b) {
+        if (gameboardArray[a][b] === '-') return true;
+    }
+
+    function getBoard() {
+        return gameboardArray;
     }
 
     display();
@@ -28,26 +32,45 @@ const gameboard = (function() {
         resetBoard,
         display,
         placeMark,
-        canPlace
+        canPlace,
+        getBoard
     }
 })();
 
 const game = (function() {
     const players = createPlayers(); 
 
+    function isGameOver(currentPlayer, a, b) {
+        const arr = gameboard.getBoard();
+        checkRow(arr, a, b);
+        if(checkRow(arr, a, b)) {
+            console.log(`${currentPlayer} wins!`);
+        };
+    }
+
+    function checkRow(arr, a, b) {
+        if (arr[a].every(checkWin)) {
+            return true;
+        }
+        function checkWin(element) {
+            return element === arr[a][b];
+        }
+    }
+
     function nextTurn() {
         players.switchPlayers();
     }
 
-    function playTurn(index) {
-        if (gameboard.canPlace(index)) {
-            gameboard.placeMark(players.getCurrentPlayer(), index);
+    function playTurn(a, b) {
+        if (gameboard.canPlace(a, b)) {
+            const currentPlayer = players.getCurrentPlayer();
+            gameboard.placeMark(currentPlayer, a, b);
+            isGameOver(currentPlayer, a, b);
             nextTurn();
         }
         else {
             console.log('That spot is already taken. Choose another.');
         }
-
     }
     return {
         playTurn,
